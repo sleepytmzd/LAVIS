6cd97a4f70f8b35c518abaacde251185ae5fb25f
sleepytmzd <depressionstonks420@gmail.com>
2026-03-13 01:18:32 +0600

    kill me

--- lavis/models/blip2_models/modeling_t5.py
+++ lavis/models/blip2_models/modeling_t5.py
@@ -63,7 +63,40 @@ from transformers.utils import (
     logging,
     replace_return_docstrings,
 )
-from transformers.utils.model_parallel_utils import assert_device_map, get_device_map
+from transformers.modeling_utils import get_device_map
+try:
+    from transformers.modeling_utils import assert_device_map
+except ImportError:
+    def assert_device_map(device_map, num_blocks):
+        blocks = list(range(0, num_blocks))
+
+        device_map_blocks = [item for sublist in list(device_map.values()) for item in sublist]
+
+        # Duplicate check
+        duplicate_blocks = []
+        for i in device_map_blocks:
+            if device_map_blocks.count(i) > 1 and i not in duplicate_blocks:
+                duplicate_blocks.append(i)
+        # Missing blocks
+        missing_blocks = [i for i in blocks if i not in device_map_blocks]
+        extra_blocks = [i for i in device_map_blocks if i not in blocks]
+
+        if len(duplicate_blocks) != 0:
+            raise ValueError(
+                "Duplicate attention blocks specified in device_map. Attention blocks must be specified to one device."
+                " These attention blocks were specified more than once: " + str(duplicate_blocks)
+            )
+        if len(missing_blocks) != 0:
+            raise ValueError(
+                "There are attention blocks for this model that are not specified in the device_map. Add these attention "
+                "blocks to a device on the device_map: " + str(missing_blocks)
+            )
+        if len(extra_blocks) != 0:
+            raise ValueError(
+                "The device_map contains more attention blocks than this model has. Remove these from the device_map:"
+                + str(extra_blocks)
+            )
+    
 from transformers.models.t5.configuration_t5 import T5Config
 
 
