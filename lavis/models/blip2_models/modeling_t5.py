e59b5ae7be70a018cb69006ae291712151271845
sleepytmzd <depressionstonks420@gmail.com>
2026-03-13 01:23:02 +0600

    :)

--- lavis/models/blip2_models/modeling_t5.py
+++ lavis/models/blip2_models/modeling_t5.py
@@ -63,7 +63,19 @@ from transformers.utils import (
     logging,
     replace_return_docstrings,
 )
-from transformers.modeling_utils import get_device_map
+
+try:
+    from transformers.modeling_utils import get_device_map
+except ImportError:
+    from math import ceil
+    def get_device_map(n_layers, devices):
+        """Returns a dictionary of layers distributed evenly across all devices."""
+        layers = list(range(n_layers))
+        n_blocks = int(ceil(n_layers / len(devices)))
+        layers_list = [layers[i : i + n_blocks] for i in range(0, n_layers, n_blocks)]
+
+        return dict(zip(devices, layers_list))
+
 try:
     from transformers.modeling_utils import assert_device_map
 except ImportError:
