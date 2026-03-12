a8c324a3341031ea4421fabc9c83df9be4800cb1
sleepytmzd <depressionstonks420@gmail.com>
2026-03-13 00:20:02 +0600

    transformers modelling_utils -> utils

--- lavis/models/med.py
+++ lavis/models/med.py
@@ -37,7 +37,13 @@ from transformers.modeling_outputs import (
     SequenceClassifierOutput,
     TokenClassifierOutput,
 )
-from transformers.modeling_utils import (
+# from transformers.modeling_utils import (
+#     PreTrainedModel,
+#     apply_chunking_to_forward,
+#     find_pruneable_heads_and_indices,
+#     prune_linear_layer,
+# )
+from transformers.utils import (
     PreTrainedModel,
     apply_chunking_to_forward,
     find_pruneable_heads_and_indices,
