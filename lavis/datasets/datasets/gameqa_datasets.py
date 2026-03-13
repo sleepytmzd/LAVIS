89c33d348ddd3c9a6489a2aad0e58b518bb1d529
sleepytmzd <depressionstonks420@gmail.com>
2026-03-13 12:02:28 +0600

    filter invalid annotations

--- lavis/datasets/datasets/gameqa_datasets.py
+++ lavis/datasets/datasets/gameqa_datasets.py
@@ -12,24 +12,33 @@ from lavis.datasets.datasets.base_dataset import BaseDataset
 
 
 class GameQAImageTextDataset(BaseDataset):
-    """Image-text dataset for GameQA stage-1 pretraining."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        valid_annotations = []
+        invalid_count = 0
+
+        for ann in self.annotation:
+            image_rel = ann.get("image")
+            caption = ann.get("state_description") or ann.get("caption")
+
+            if image_rel and caption:
+                valid_annotations.append(ann)
+            else:
+                invalid_count += 1
+        
+        print(f"Filtered out {invalid_count} invalid annotations. {len(valid_annotations)} valid annotations remain.")
+
+        self.annotation = valid_annotations
 
     def __getitem__(self, index):
         ann = self.annotation[index]
 
-        image_rel = ann.get("image")
-        if not image_rel:
-            return None
-
-        image_path = os.path.join(self.vis_root, image_rel)
-        try:
-            image = Image.open(image_path).convert("RGB")
-        except Exception:
-            return None
+        image_path = os.path.join(self.vis_root, ann["image"])
+        image = Image.open(image_path).convert("RGB")
 
         caption = ann.get("state_description") or ann.get("caption")
-        if caption is None:
-            return None
 
         image = self.vis_processor(image)
         caption = self.text_processor(caption)
