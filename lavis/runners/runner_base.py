39500175a1922ba80abba1b7017ad0ff474817d9
sleepytmzd <depressionstonks420@gmail.com>
2026-03-15 12:05:43 +0600

    remove checkpointing after every train epoch

--- lavis/runners/runner_base.py
+++ lavis/runners/runner_base.py
@@ -408,9 +408,10 @@ class RunnerBase:
                             self.log_stats(val_log, split_name)
 
             else:
+                pass
                 # if no validation split is provided, we just save the checkpoint at the end of each epoch.
-                if not self.evaluate_only:
-                    self._save_checkpoint(cur_epoch, is_best=False)
+                # if not self.evaluate_only:
+                    # self._save_checkpoint(cur_epoch, is_best=False)
 
             if self.evaluate_only:
                 break
